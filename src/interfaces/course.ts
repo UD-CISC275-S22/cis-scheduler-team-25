3837b9573d7507d830531e1829d5bcbc67df2555
bgallamoza <brennan.gallamoza@gmail.com>
2022-04-07 15:02:47 -0400

    added comments to interfaces, created new coreq field for Course interface

--- src/interfaces/course.ts
+++ src/interfaces/course.ts
@@ -1,9 +1,16 @@
 import { Section } from "./section";
 export interface Course {
+    // name of course
     name: string;
+    // credit value for this course
     credits: number;
     // Prereqs: course ID numbers
     prereqs: number[];
+    // coreqs: course ID numbers
+    coreqs: number[];
+    /* numeric code for a course, e.g. 1.181 corresponds to CISC181,
+    where CISC = 1 and .181 identifies the department course number */
     courseCode: number;
+    // Array of sections for this course
     sections: Section[];
 }
