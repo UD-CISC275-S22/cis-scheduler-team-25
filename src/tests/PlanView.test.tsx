c58579963e6f3ed1d69e61a4128e661d5c9ac23a
Faizel Quabili <faizelq@udel.edu>
2022-04-12 16:07:30 -0400

    added remove-all-semesters data-testid test (fails currently)

--- src/tests/PlanView.test.tsx
+++ src/tests/PlanView.test.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 import userEvent from "@testing-library/user-event";
-
 describe("PlanView Tests", () => {
     beforeEach(() => {
         render(<App />);
@@ -28,6 +27,16 @@ describe("PlanView Tests", () => {
         expect(
             screen.queryByText("Naruto's Degree Plan 1")
         ).not.toBeInTheDocument();
+    });
+    test("There is a button for removing all semesters", () => {
+        const removeAllSemesters = screen.getByTestId(
+            "remove-all-semesters-button"
+        );
+
+        expect(screen.getByText("Remove All Semesters")).toBeInTheDocument();
+        removeAllSemesters.click();
+        expect(screen.queryByText("F-2022")).not.toBeInTheDocument();
+        expect(screen.queryByText("S-2023")).not.toBeInTheDocument();
     });
     test("Different header is displayed for a different degree plan", () => {
         screen.getByTestId("plan-main-button").click();
