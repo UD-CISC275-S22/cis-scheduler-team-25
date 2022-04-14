9d2e7e81ccb45f77916b13f9447d70268b3f215c
Faizel Quabili <faizelq@udel.edu>
2022-04-14 15:06:50 -0400

    remove all courses - errors: red underlines in app.tsx, and if you were to clear out courses then switch to another degree plan and then come back the courses come back

--- src/components/CurrentView.tsx
+++ src/components/CurrentView.tsx
@@ -46,6 +46,8 @@ export function CurrentView({
                     setMode={setMode}
                     setCurrentSemester={setCurrentSemester}
                     currentSemester={currentSemester}
+                    setCurrentPlan={setCurrentPlan}
+                    currentPlan={currentPlan}
                 />
             );
         default:
--- src/components/SemesterView.tsx
+++ src/components/SemesterView.tsx
@@ -1,5 +1,7 @@
+import { check } from "prettier";
 import React from "react";
 import { Button } from "react-bootstrap";
+import { DegreePlan } from "../interfaces/degreeplan";
 import { Semester } from "../interfaces/semester";
 
 // Button for returning back to the PlanView
@@ -20,17 +22,37 @@ function PlanViewButton({
 
 function RemoveAllCourses({
     setCurrentSemester,
+    setCurrentPlan,
+    //setPlans,
+    currentPlan,
+    //plans,
     currentSemester
 }: {
     setCurrentSemester: (currentSemester: Semester) => void;
+    setCurrentPlan: (currentPlan: DegreePlan) => void;
+    //setPlans: (plans: DegreePlan[]) => void;
+    currentPlan: DegreePlan;
     currentSemester: Semester;
+    //plans: DegreePlan[];
 }): JSX.Element {
     return (
         <Button
             data-testid="remove-all-courses-from-semester"
             className="mode-button"
             onClick={() => {
-                setCurrentSemester({ ...currentSemester, courses: [] });
+                const newSemester = { ...currentSemester, courses: [] };
+                const updateSemesters = currentPlan.semesters.map(
+                    (checkSemester: Semester): Semester =>
+                        checkSemester.season === newSemester.season
+                            ? newSemester
+                            : checkSemester
+                );
+                const newDegreePlan = {
+                    ...currentPlan,
+                    semesters: updateSemesters
+                };
+                setCurrentSemester(newSemester);
+                setCurrentPlan(newDegreePlan);
             }}
         >
             Remove All Courses
@@ -46,11 +68,18 @@ selected classes
 export function SemesterView({
     setMode,
     setCurrentSemester,
+    setCurrentPlan,
+    //setPlans,
+    currentPlan,
+    //plans,
     currentSemester
 }: {
     setMode: (newMode: string) => void;
     setCurrentSemester: (currentSemester: Semester) => void;
+    setCurrentPlan: (currentPlan: DegreePlan) => void;
     currentSemester: Semester;
+    //setPlans: (plans: DegreePlan[]) => void;
+    currentPlan: DegreePlan;
 }): JSX.Element {
     return (
         <div>
@@ -65,6 +94,8 @@ export function SemesterView({
             </div>
             <RemoveAllCourses
                 setCurrentSemester={setCurrentSemester}
+                setCurrentPlan={setCurrentPlan}
+                currentPlan={currentPlan}
                 currentSemester={currentSemester}
             ></RemoveAllCourses>
             <div></div>
