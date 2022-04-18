cfc93e860e5559108ab40728c1da960a223b0694
bgallamoza <brennan.gallamoza@gmail.com>
2022-04-18 11:29:06 -0400

    resolved linting errors

--- src/components/dropLogic/GroupRadioButtons.tsx
+++ src/components/dropLogic/GroupRadioButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Course } from "../../interfaces/course";
 import { Form } from "react-bootstrap";
 import "../components.css";
