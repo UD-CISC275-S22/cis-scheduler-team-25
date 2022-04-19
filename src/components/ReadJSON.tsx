e2dcd7ca1eebd1cace3b32f7273dc44b6c89353b
bgallamoza <brennan.gallamoza@gmail.com>
2022-04-18 20:15:23 -0400

    added comments ReadJSON

--- src/components/ReadJSON.tsx
+++ src/components/ReadJSON.tsx
@@ -2,6 +2,7 @@ import { Course } from "../interfaces/course";
 import catalogData from "../exampleData/catalog.json";
 import courseCategoriesData from "../exampleData/category_courses.json";
 
+// initial interface for the information in catalog.json
 export interface CatalogCourse {
     code: string;
     name: string;
@@ -16,10 +17,15 @@ export interface CatalogCourse {
 // read in catalog.json as a HashMap of Hashmaps to courses
 const catalog = catalogData as Record<string, Record<string, CatalogCourse>>;
 
+// import a record of courseCategoriesData, containing a record where keys
+// represent degreeRequirements whose values are a list of course code strings
 const courseCategories = courseCategoriesData as Record<string, string[]>;
 
+// Get array of degree requirement categories
 const categories = Object.keys(courseCategories);
 
+// map each degree category to a list of Course objects based on the course codes.
+// Course codes are used to obtain the Course objects in the catalog
 const courseArrList = categories.map((category: string): Course[] =>
     courseCategories[category].map(
         (code: string): Course => ({
@@ -29,6 +35,7 @@ const courseArrList = categories.map((category: string): Course[] =>
     )
 );
 
+// Turn array of array of Courses into a single of array of Courses
 const unfilteredCourseList = courseArrList.reduce(
     (fullList: Course[], currentList: Course[]) => [
         ...fullList,
@@ -37,10 +44,14 @@ const unfilteredCourseList = courseArrList.reduce(
     []
 );
 
+// unfilteredCourseList has duplicate course codes because a single course
+// can be in multiple degreeRequirements, so we first get all unique course codes
 const uniqueCourseCodes = Array.from(
     new Set(unfilteredCourseList.map((course: Course): string => course.code))
 );
 
+// combine all duplicates into a single Course by merging all of the
+// degreeCategories into a single list
 const courseList = uniqueCourseCodes.map(
     (code: string): Course =>
         unfilteredCourseList
