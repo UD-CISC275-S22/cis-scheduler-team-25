162c612c17724844023b161406a0088fb265ca2d
Faizel Quabili <faizelq@udel.edu>
2022-04-21 20:29:10 -0400

    started code

--- src/components/EditPlanForm.tsx
+++ src/components/EditPlanForm.tsx
@@ -2,3 +2,15 @@ import React, { useState } from "react";
 import { Button, Form, Row, Col } from "react-bootstrap";
 import { DegreePlan } from "../interfaces/degreeplan";
 import "./components.css";
+
+function removePlanByName(
+    plans: DegreePlan[],
+    setPlans: (newPlans: DegreePlan) => void,
+    currentPlan: DegreePlan
+): void {
+    const removedPlans = plans.filter(
+        (currPlan: DegreePlan): boolean => currPlan.id === currentPlan.id
+    );
+
+    setPlans(removedPlans);
+}
