aecd05481e3c10979ca98c1e3188aade995b9611
Faizel Quabili <faizelq@udel.edu>
2022-05-11 10:11:45 -0400

    updates

---
+++ src/tests/ImportCSV.test.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+import examplePlan from "./exampledata/Example_Degree_Plan.csv";
